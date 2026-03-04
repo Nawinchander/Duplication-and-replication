3ccbe88c3ca0498afa92a24441d4ba7005be3e0d
Nawin chander <nawinnapzy289@gmail.com>
2026-03-04 21:30:38 +0530

    new code

--- Replication/Multi_region_replication/script.js
+++ Replication/Multi_region_replication/script.js
@@ -8,3 +8,26 @@
 // (Like global databases in real systems such as Amazon Web Services or Google Cloud)
 
 
+let primaryRegion = { orders: [] };
+let indiaRegion = {};
+let usRegion = {};
+let europeRegion = {};
+
+function globalReplication() {
+  indiaRegion = JSON.parse(JSON.stringify(primaryRegion));
+  usRegion = JSON.parse(JSON.stringify(primaryRegion));
+  europeRegion = JSON.parse(JSON.stringify(primaryRegion));
+}
+
+function createOrder(order) {
+  primaryRegion.orders.push(order);
+  globalReplication();
+}
+
+globalReplication();
+
+createOrder("Order-101");
+
+console.log("India:", indiaRegion.orders);
+console.log("US:", usRegion.orders);
+console.log("Europe:", europeRegion.orders);
