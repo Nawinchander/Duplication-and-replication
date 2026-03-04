ccb89470e92a37b83c68ad3f27bc31cbce940fb0
Nawin chander <nawinnapzy289@gmail.com>
2026-03-04 21:27:37 +0530

    new file

--- duplication/Data Export/script.js
+++ duplication/Data Export/script.js
@@ -2,3 +2,28 @@
 // After export → no automatic sync
 
 
+let mainUsers = [
+  { id: 1, name: "Nawin" },
+  { id: 2, name: "Ironman" }
+];
+
+let reportingSystem = [];
+
+// Manual export
+function exportData() {
+  reportingSystem = JSON.parse(JSON.stringify(mainUsers));
+  console.log("Data exported!");
+}
+
+exportData();
+
+console.log("Reporting Data:", reportingSystem);
+
+// Change main system
+mainUsers.push({ id: 3, name: "MBA Engineer" });
+
+console.log("After change:");
+console.log("Main:", mainUsers);
+console.log("Reporting:", reportingSystem);
+
+
