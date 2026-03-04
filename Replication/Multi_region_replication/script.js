707a5f80bfd9fe9a5c5bdcd372c52bd6160d8e7c
Nawin chander <nawinnapzy289@gmail.com>
2026-03-04 21:30:07 +0530

    add new file

---
+++ Replication/Multi_region_replication/script.js
@@ -0,0 +1,10 @@
+// You have servers in:
+
+// India
+// US
+// Europe
+
+// Write once → replicate everywhere.
+// (Like global databases in real systems such as Amazon Web Services or Google Cloud)
+
+
